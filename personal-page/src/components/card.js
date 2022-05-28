410fd55996df5c4a0f21baf49788d7dbb05b5a47
Daniel Lisko <djlisko01@Daniels-MacBook-Air.local>
2022-05-28 13:26:54 -0700

    added cards to projects

--- personal-page/src/components/card.js
+++ personal-page/src/components/card.js
@@ -1,38 +1,41 @@
 import { useState } from "react";
+import { Button } from "react-bootstrap";
 
 function Card() {
   const [contentDisplayed, setContentDisplayed] = useState(false);
 
   const cardEntered = () => {
-    console.log("hover");
     setContentDisplayed(true);
   };
 
   const cardExited = () => {
-    console.log("left");
     setContentDisplayed(false);
   };
 
   return (
     <div className="card" onMouseEnter={cardEntered} onMouseLeave={cardExited}>
-      <span
-        className={`card-title ${contentDisplayed ? "overlay" : ""}`}
+      <div
+        className={`${contentDisplayed ? "overlay" : ""}`}
         style={{
           "background-color": "rgb(79, 93, 117, 0.8)",
-          padding: 0,
           "background-image": `url("../../public/imgs/water_sprite_lake.JPG")`,
         }}
       >
-        <h3>Project Title</h3>
-        <div className="card-content">
-          {contentDisplayed ? (
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Delectus, aliquid?
-            </p>
-          ) : null}
+        <div className="card-title">
+          <h3>Project Title</h3>
         </div>
-      </span>
+        {contentDisplayed ? (
+          <div className="card-content">
+            <div className="card-breifing">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Delectus, aliquid?
+              </p>
+            </div>
+            <Button className="details">More Details</Button>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
--- personal-page/src/components/pages/projects.js
+++ personal-page/src/components/pages/projects.js
@@ -1,16 +1,23 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Card from "../card.js";
+
 function Projects() {
+  let cards = [];
+
+  for (let i = 0; i < 5; i++) {
+    cards.push(
+      <Col lg={4} style={{ padding: "2px" }}>
+        <Card />
+      </Col>,
+    );
+  }
+
   return (
     <div id="projects">
       <Container>
-        <Row>
-          <Col>
-            <h2>Projects</h2>
-            <div>
-              <Card />
-            </div>
-          </Col>
+        <Row style={{ "padding-top": "5rem" }}>
+          <h2>Projects</h2>
+          {cards}
         </Row>
       </Container>
     </div>
